f0e9c646e737dbfed9025b656233d18bfc5dfce0
kadajett <jeremy.ryan.stover@gmail.com>
2018-08-21 13:18:26 -0700

    added hashing to improve speed

---
+++ index.js
@@ -0,0 +1,53 @@
+class DatHash {
+    constructor() {
+        this.list = [];
+    }
+
+    get(x) {
+        let result;
+        let i = this.hash(x);
+
+        if(!this.list[i]) {
+            return undefined;
+        }
+
+        this.list[i].forEach(pair => {
+            if (pair[0] === x) {
+                result = pair[1];
+            }
+        });
+
+        return result;
+    }
+
+    set(x, y) {
+        let i = this.hash(x);
+        if(!this.list[i]) {
+            this.list[i] = []
+        }
+        this.list[i].push([x, y]);
+    }
+
+    hash(key) {
+        let hash = 0;
+        if (key.length == 0) {
+            return hash;
+        }
+        for (let i = 0; i < key.length; i++) {
+            let char = key.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash = hash & hash;
+        }
+        return hash;
+    }
+}
+
+let h = new DatHash();
+
+for (let i = 1000000; i >= 0; i--) {
+    h.set(`element${i}`, i);
+}
+
+console.time('with lots of records in the map')
+h.get('I_DONT_EXIST')
+console.timeEnd('with lots of records in the map')
